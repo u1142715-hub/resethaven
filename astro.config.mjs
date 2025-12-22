5b925029c5b1b2dd72f203d03fef3ec094d9019d
u1142715-hub <u1142715@umail.usq.edu.au>
2025-12-22 15:56:32 +1000

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://github.com/u1142715-hub/resethaven/",
+  site: "https://u1142715-hub.github.io/resethaven/",
+
   base: "/resethaven",
 
   trailingSlash: "always",
